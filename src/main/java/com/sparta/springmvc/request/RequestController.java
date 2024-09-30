b40dc08b443e6d4dd5c7c41a26ba39c66fc2e1a0
ethrad <asdf123f4f4@gmail.com>
2024-09-30 21:26:41 +0900

    Path Variable과 Request Param

---
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -0,0 +1,49 @@
+package com.sparta.springmvc.request;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/hello/request")
+public class RequestController {
+    @GetMapping("/form/html")
+    public String helloForm() {
+        return "hello-request-form";
+    }
+
+    // [Request sample]
+    // path variable 방식
+    // 주소에 데이터가 있음
+    // GET http://localhost:8080/hello/request/star/Robbie/age/95
+    @GetMapping("/star/{name}/age/{age}")
+    @ResponseBody
+    public String helloRequestPath(@PathVariable String name, @PathVariable int age) {
+        return String.format("Hello, @PathVariable.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // request param 방식
+    // param?key=value&key=value
+    // GET http://localhost:8080/hello/request/form/param?name=Robbie&age=95
+    @GetMapping("/form/param")
+    @ResponseBody
+    // (required = false) name이 비어 있어도 오류 안 남
+    // default는 required = true
+    public String helloGetRequestParam(@RequestParam(required = false) String name, int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/param
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    @PostMapping("/form/param")
+    @ResponseBody
+    public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
+        return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+
+}
